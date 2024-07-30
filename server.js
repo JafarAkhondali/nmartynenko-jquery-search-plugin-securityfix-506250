a6b1f5e93cda2c3ccdf21592074371718b2e07cd
Jafar Akhondali <jafar.akhoondali@gmail.com>
2024-07-30 18:58:41 +0200

    Block malicious looking requests to prevent path traversal attacks.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ console.log('This is Node.JS server launched for demo purposes. DO NOT USE IN PR
 
 http.createServer(function(request, response) {
 	var uri = url.parse(request.url).pathname;
+	if (path.normalize(decodeURI(uri)) !== decodeURI(uri)) {
+		response.statusCode = 403;
+		response.end();
+		return;
+	}
 
 	//handle most common URLs
 	if (uri == "/" || uri == "/index.html") {
